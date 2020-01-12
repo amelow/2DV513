64f1e2973012563b8728040d3bb77bfcf1e04724
amelow <amelielowe96@gmail.com>
2020-01-12 11:29:38 +0100

    connecting with the databases

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -5,6 +5,7 @@ const app = express()
 // finns en environment variabel annars default port
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
+var mysql = require('mysql')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -12,7 +13,24 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.post('/book', (request, response) => {
-  // response.json(request.body)
   console.log(request.body)
+  createConnection(request)
 })
+function createConnection (request) {
+  var con = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Ulrik123456',
+    database: 'sys'
+  })
+  con.connect(function (err) {
+    if (err) {
+      console.log('IT DIDNT WORK')
+      throw err
+    } else {
+      console.log('Connected!')
+    }
+  })
+}
 app.listen(port, console.log('server starts'))
