4bd86f652aeb6283e81a37fb5e722d2637d90c8d
amelow <amelielowe96@gmail.com>
2020-01-12 12:30:14 +0100

    fetching data works

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -6,6 +6,9 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 var mysql = require('mysql')
+var fileSystem = require('fs')
+var fileReader = require('readline')
+var bookInformation = []
 
 var con = mysql.createConnection({
   host: '127.0.0.1',
@@ -32,6 +35,12 @@ app.post('/book', (request, response) => {
   fetchData(request)
 })
 function fetchData (request) {
-  console.log(request.body)
+  bookInformation = [[request.body.bookTitle, request.body.author, request.body.ratings]]
+  console.log('BOOK INFO ' + bookInformation)
+  var sqlTable1 = 'INSERT IGNORE INTO BookInfo (bookTitle, authorName, bookRating ) VALUES ?'
+  con.query(sqlTable1, [bookInformation], function (err, result) {
+    if (err) throw err
+  })
 }
+
 app.listen(port, console.log('server starts'))
